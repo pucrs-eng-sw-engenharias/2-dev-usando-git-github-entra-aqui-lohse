5e6b5ba2c718bbb507d0c7b8891fc35f39848e48
felipechitolina <62725733+felipechitolina@users.noreply.github.com>
2020-09-17 19:04:51 -0300

    lendo o txt
    ja le o txt falta pegar os valores

--- Mars Rover/main.c
+++ Mars Rover/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main()
 {
-    printf("Mars Rover!\n");
+    FILE *arquivo_txt;
+    char *resultado, texto_da_linha[50], comando[20], heading;
+    int linha, m=0, n=0, x=0, y=0;
+
+
+    arquivo_txt = fopen("rover.txt","rt");
+     if(arquivo_txt == NULL)
+    {
+        printf("Erro na leitura");
+    }
+    linha=1;
+    printf("Mars Rover!\n\n");
+    while(!feof(arquivo_txt))
+    {
+        resultado = fgets(texto_da_linha,100,arquivo_txt);
+        if(resultado)
+        {
+            printf("linha %d: %s",linha,texto_da_linha);
+            linha++;
+        }
+    }
+
     return 0;
 }
