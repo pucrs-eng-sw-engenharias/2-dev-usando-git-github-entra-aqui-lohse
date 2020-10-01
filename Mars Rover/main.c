58e1ec83ac158a699431bfc361c362c5b4649e33
matheuslohse <62487417+matheuslohse@users.noreply.github.com>
2020-10-01 19:33:43 -0300

    Falta pouco

--- Mars Rover/main.c
+++ Mars Rover/main.c
@@ -9,8 +9,7 @@ int main()
     char *resultado, texto_da_linha[50], heading;
     char comando[20];
     int linha, m=0, n=0, x=0, y=0;
-    int tamanho_do_tabuleiro_x = 0, tamanho_do_tabuleiro_y = 0, area = 0;
-
+    int tamanho_do_tabuleiro_x = 0, tamanho_do_tabuleiro_y = 0, area = 0, i = 0, tamanho_comando= 0;
 
     arquivo_txt = fopen("rover.txt","rt");
      if(arquivo_txt == NULL)
@@ -29,65 +28,47 @@ int main()
                 tamanho_do_tabuleiro_x = (int)*texto_da_linha - 48;
                 tamanho_do_tabuleiro_y = (int)*(texto_da_linha+2) - 48;
                 area = tamanho_do_tabuleiro_x*tamanho_do_tabuleiro_y;
-                printf("\n----%d m^2----\n\n",area);
+                printf("\nA area do tabuleiro e de: %d m^2\n",area);
             }
             linha++;
              if(linha == 4){
                 x = (int)*texto_da_linha - 48;
-                y = (int)*(texto_da_linha+2) - 48;
-                heading = (texto_da_linha+4);
+                y = (int)*(texto_da_linha + 2) - 48;
+                heading = *(texto_da_linha + 4);
+                // printf("\n%c\n",heading);
             }
               if(linha == 6){
-
-
-                printf("\n\n********%s*********\n\n",&texto_da_linha[2]);
-
-
+                strcpy(comando, texto_da_linha);
+                tamanho_comando = strlen(comando) - 1;
+                for(i = 0; i<tamanho_comando; i++){
+                    printf("\nComando(%i) = %c\n",i, *(comando + i));
+                /*
+                    if(strcmp(heading,"N") == 0){
+                        if(strcmp(*(comando + i),"M") == 0) y++;
+                        if(strcmp(*(comando + i),"L") == 0) heading = "W";
+                        if(strcmp(*(comando + i),"R") == 0) heading = "E";
+                    }
+                    if(heading == "W"){
+                        if(strcmp(*(comando + i),"M") == 0) x--;
+                        if(strcmp(*(comando + i),"L") == 0) heading = "S";
+                        if(strcmp(*(comando + i),"R") == 0) heading = "N";
+                    }
+                    if(heading == "S"){
+                        if(*(comando + i) == "M") y--;
+                        if(*(comando + i) == "L") heading = "E";
+                        if(*(comando + i) == "R") heading = "W";
+                    }
+                    if(heading == "E"){
+                        if(*(comando + i) == "M") x++;
+                        if(*(comando + i) == "L") heading = "N";
+                        if(*(comando + i) == "R") heading = "S";
+                    }
+                    else printf("\nNao entrou em ninguem\n");
+                */
+                }
             }
         }
     }
-
-    printf("\n\nMars Rover!\n");
-    char posicao[] = "1 2 N";
-    printf("\n%c", *posicao);
-    printf("\n%c", *(posicao + 2));
-    x = *posicao;
-    y = *(posicao + 2);
-    heading = *(posicao + 4);
-
-    char temp[20] = "MLMR";
-    strcpy(comando, temp);
-    printf("\n%c", *(comando + 0));
-
-    int retorno;
-    retorno = strcmp(heading,"N");
-    printf("%i",&retorno);
-
-
-        for(int i=0; i<sizeof(comando); i++){
-            if(heading == 'N'){
-                if(*(comando + i) == 'M') y++;
-                if(*(comando + i) == 'L') heading = "W";
-                if(*(comando + i) == 'R') heading = "E";
-            }
-            if(heading == 'W'){
-                if(*(comando + i) == 'M') x--;
-                if(*(comando + i) == 'L') heading = "S";
-                if(*(comando + i) == 'R') heading = "N";
-            }
-            if(heading == 'S'){
-                if(*(comando + i) == "M") y--;
-                if(*(comando + i) == "L") heading = "E";
-                if(*(comando + i) == "R") heading = "W";
-            }
-            if(heading == 'E'){
-                if(*(comando + i) == "M") x++;
-                if(*(comando + i) == "L") heading = "N";
-                if(*(comando + i) == "R") heading = "S";
-            }
-            printf("\nA contagem esta em %i e o robo esta virado para ",&i);
-            puts(heading);
-        }
 
     return 0;
 }
